5e605686fbc0924453774406213cd9b136903b55
Benny Peckruhn <benny@bennyville.de>
2020-07-02 19:54:47 +0200

    list all posts on index

--- src/lib/blog_web/controllers/page_controller.ex
+++ src/lib/blog_web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule BlogWeb.PageController do
   use BlogWeb, :controller
 
+  import Ecto.Query
+
+  alias Blog.Posts.Post
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    posts = Blog.Repo.all(from p in Post,
+      limit: 5,
+      order_by: [desc: :inserted_at],
+      preload: [:user])
+
+    render(conn, "index.html", posts: posts)
   end
 end
